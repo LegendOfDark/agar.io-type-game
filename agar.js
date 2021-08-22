ea8dee94c1caf0d3f5a85ce5645ec94a7b8869f5
LegendOfDark <67946587+LegendOfDark@users.noreply.github.com>
2021-08-22 02:10:26 -0400

    added spikes and improved overall userability of game

--- agar.js
+++ agar.js
@@ -35,7 +35,6 @@ document.addEventListener('keydown', (e) =>
     keysPressed[e.key] = true;
     character.char_update();
 });
-
 document.addEventListener('keyup', (e) => {
     keysPressed[e.key] = false;
  });
@@ -48,9 +47,24 @@ let dstnc = (x1, y1, x2, y2) =>{
     return Math.hypot(dstnc_x, dstnc_y);
 }
 
+// score count and game_end banner
+let score = 0;
+let game_flag = false;
+let drawScore = () => {
+    c.font = "25px Arial";
+    c.fillStyle = "#0095DD";
+    c.textAlign = "left";
+    c.fillText("Score: "+ score, canvas.width - 150, 50);
+}
+let game_end = () => {
+    c.font = "100px Arial";
+    c.fillStyle = "#000000";
+    c.textAlign = "center";
+    c.fillText("Game Ended", canvas.width/2, canvas.height/2);
+}
+
 // create a circle object
 function Circle(x, y, rad, color) {
-
     this.x = x;
     this.y = y;
     this.rad = rad;
@@ -69,44 +83,39 @@ function Circle(x, y, rad, color) {
         c.stroke();
     }
 
+    // character interactivity
     this.char_update = () => {
 
         // based on the key pressed, move the circle in a certain direction
 
-        let speed = 7; 
+        let speed = 5; 
 
         if (keysPressed['ArrowRight'])
         {
-            this.dx = speed;
-            this.dy = 0;
+            this.x += speed;
         }
         if (keysPressed['ArrowLeft'])
         {
-            this.dx = -speed;
-            this.dy = 0;
+            this.x += -speed;
         }
         if (keysPressed['ArrowUp'])
         {
-            this.dy = -speed;
-            this.dx = 0;
+            this.y += -speed;
         }
         if (keysPressed['ArrowDown'])
         {
-            this.dy = speed;
-            this.dx = 0;
+            this.y += speed;
         }   
 
         // check if the ball touches the walls
         if (this.x + this.rad + 3 > canvas.width || this.x - this.rad - 3 < 0) {
+            console.log('affirmative');
             this.dx = -this.dx;
         }
         if (this.y + this.rad + 3 > canvas.height || this.y - this.rad - 3 < 0) {
+            console.log('affirmative');
             this.dy = -this.dy;
         }
-
-
-        this.x += this.dx;
-        this.y += this.dy;
 
         char_x = this.x;
         char_y = this.y;
@@ -115,10 +124,14 @@ function Circle(x, y, rad, color) {
 
     }
 
+    // food interactivity
     this.food_update = () => {
         // console.log(dstnc(this.x, this.y, char_x, char_y));
-        if (dstnc(this.x, this.y, char_x, char_y) < this.rad + char_radius){
+        if (dstnc(this.x, this.y, char_x, char_y) < this.rad + character.rad){
             this.flag = true;
+            score++;
+            character.rad += 0.2;
+
         }
         else{
             this.flag = false;
@@ -127,57 +140,148 @@ function Circle(x, y, rad, color) {
     }
 }
 
+// create a spikes / stars object
+function drawStar(cx, cy, spikes, outerRadius, innerRadius){
+    this.rot = Math.PI / 2 * 3;
+    this.cx = cx;
+    this.cy = cy;
+    this.x = this.cx;
+    this.y = this.cy;
+    this.spikes = spikes;
+    this.step = Math.PI / spikes;
+    this.outerRadius = outerRadius;
+    this.innerRadius = innerRadius;    
+    this.dx = (Math.random() - 0.5) * 10;
+    this.dy = (Math.random() - 0.5) * 10;
 
-// make the chracter
+    this.draw = () => {
+        c.strokeSyle = "#000";
+        c.beginPath();
+        c.moveTo(this.cx, this.cy - this.outerRadius)
+        for (i = 0; i < this.spikes; i++) {
+            this.x = this.cx + Math.cos(this.rot) * this.outerRadius;
+            this.y = this.cy + Math.sin(this.rot) * this.outerRadius;
+            c.lineTo(this.x, this.y)
+            this.rot += this.step
+
+            this.x = this.cx + Math.cos(this.rot) * this.innerRadius;
+            this.y = this.cy + Math.sin(this.rot) * this.innerRadius;
+            c.lineTo(this.x, this.y)
+            this.rot += this.step
+        }
+        c.lineTo(this.cx, this.cy - this.outerRadius)
+        c.closePath();
+        c.lineWidth = 5;
+        c.strokeStyle='blue';
+        c.stroke();
+        c.fillStyle='skyblue';
+        c.fill();
+    }
+    
+
+    this.update = () => {
+
+        if (this.cx + this.outerRadius + 25 > canvas.width || this.cx - this.outerRadius - 25  < 0){
+            this.dx = -this.dx;
+            // console.log(this.dx);
+        }
+        if (this.cy + this.outerRadius + 20 > canvas.height || this.cy - this.outerRadius - 20 < 0){
+            this.dy = -this.dy;
+        }
+        this.cx += this.dx;
+        this.cy += this.dy;
+        this.draw();
+        // spike interactivity
+        if (dstnc(this.x, this.y, char_x, char_y) < this.outerRadius + character.rad){
+            game_flag = true;
+        }
+    }
+}
+
+
+// make the character
 let char_x = 0;
 let char_y = 0;
 let char_radius = 30;
+var character = new Circle( Math.random() * (canvas.width - char_radius * 2) + char_radius, Math.random() * (canvas.height - char_radius * 2) + char_radius, char_radius, randomColor())
 
-var character = new Circle( Math.random() * (canvas.width - char_radius * 2) + char_radius, 
-                            Math.random() * (canvas.height - char_radius * 2) + char_radius,
-                            char_radius, randomColor())
-
+// intialize extra variables used in functions
 var foodArr = [];
+let starArr = [];
+let flag = null;
 
 // function creates a certain # of food
+var food_num;
 function init() {
 
   foodArr = [];
+  starArr = [];
+
+  for (let j = 0; j < 5; j++){
+    var outerRadius = 30;
+    var x = Math.random() * (canvas.width - outerRadius * 2) + outerRadius;
+    var y = Math.random() * (canvas.height - outerRadius * 2) + outerRadius;
+    var spikes = 20;
+    var innerRadius = 25;
+    starArr.push(new drawStar(x, y, spikes, outerRadius, innerRadius));
+  }
 
   // intialize random properties of the cirle
-  for (let i = 0; i < 500; i++) {
-    var rad = (Math.random() * 5) + 1;
+  food_num = 100;
+  for (let i = 0; i < food_num; i++) {
+    var rad = (Math.random() * 5.5) + 2.5;
     var x = Math.random() * (canvas.width - rad * 2) + rad;
     var y = Math.random() * (canvas.height - rad * 2) + rad;
-    var dx = (Math.random() - 0.5) * 5;
-    var dy = (Math.random() - 0.5) * 5;
     var color = randomColor();
 
     // save the food to the Array
     foodArr.push(new Circle(x, y, rad, color));
-    // console.log(i, 'and', foodArr[i]);
     }
 }
 
-
-let flag = null;
 
 // animate the canvas by constantly reloadng the page
 let animate = () => {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < foodArr.length; i++) {
-    foodArr[i].draw();
-    foodArr[i].food_update();
-    flag = foodArr[i].flag;
-    console.log(flag);
-    if (flag){
-        foodArr = foodArr.filter(food => food.flag != true);
-        flag = false;
+    //sets animation
+    requestAnimationFrame(animate);
+
+    // clear the screen after each frame
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    // creates spikes on screen
+    for (let d = 0; d < starArr.length; d++){
+        starArr[d].update();
     }
-  } 
-  character.char_update();
+    
+    // update scorebaord
+    drawScore();
+    
+    // creates food on screen
+    for (var i = 0; i < foodArr.length; i++) {
+        foodArr[i].draw();
+        foodArr[i].food_update();
+        
+        
+        // checks if food is eaten
+        flag = foodArr[i].flag;
+        if (flag){
+            foodArr = foodArr.filter(food => food.flag != true);
+            flag = false;
+        }
+    }
+ 
+    // When game ends...
+    if (foodArr.length == 0 && score == food_num || game_flag)
+    {
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        game_end();
+        setTimeout("location.reload(true);", 7000);
+    } 
+    else{
+        character.char_update();
+    }
 }
 
+// intial call of functions
 animate();
 init();
